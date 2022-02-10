f7bfc68aafe31066872f21902e94178f7a2147ac
Gabriel Machado @DevLaw <97376083+secdevdf@users.noreply.github.com>
2022-02-10 15:19:49 -0300

    Update controller.js

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -1,11 +1,7 @@
-window.onload = () => {
-  const navMenu = document.querySelector('.nav-menu');
-  const navItems = document.querySelectorAll('.nav-item');
-  const hamburger = document.querySelector('.nav-toggle');
-  
-  const toggle = e => e.classList.toggle('is-active');
-  const toggleNav = ({ target }) => Array.from(navMenu.classList).includes('is-active') ? toggle(navMenu) : null;
+$(document).ready(() => {
+  $(() => {
+    $('[data-toggle="tooltip"]').tooltip();
+  });
 
-  hamburger.addEventListener('click', () => toggle(navMenu, 'is-active'));
-  Array.from(navItems).forEach(e => e.addEventListener('click', toggleNav));
-}
+  particlesJS.load('particles-js', 'particlesjs.json');
+});
